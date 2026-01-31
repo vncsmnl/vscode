d2a8028fc84baab7ae1af549da6bf4e12ae4d090
Vinícius Manoel  <59481808+vncsmnl@users.noreply.github.com>
2026-01-31 16:38:44 -0300

    :rocket: upd: enhance deployment workflow and update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   // App Router is now stable in Next.js 14
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? '/vscode' : '',
+  assetPrefix: isProd ? '/vscode/' : '',
   images: {
     unoptimized: true
   }
