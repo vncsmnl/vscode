9dea13a4fc051a3068263b5f73312a78e70c64cc
Vinícius Manoel  <59481808+vncsmnl@users.noreply.github.com>
2026-02-01 15:25:47 -0300

    fix: update Gist URLs in Extensions page and streamline environment variable assignments

--- src/app/vscode/extensions/page.tsx
+++ src/app/vscode/extensions/page.tsx
@@ -6,5 +6,5 @@ export const metadata = {
 
 export default async function Extensions() {
   {/* @ts-expect-error Server Component */ }
-  return <GistContent gistUrl="https://gist.githubusercontent.com/vncsmnl/80cd621c15854c98c7344097ed231c6f/raw/c1cfa85a92a85948fb7c6ee66348f050229de319/extensions.json" />
+  return <GistContent gistUrl="https://gist.githubusercontent.com/vncsmnl/80cd621c15854c98c7344097ed231c6f/raw/abca3419a80a310d2899ad921bf629abd7b7b488/extensions.json" />
 }
